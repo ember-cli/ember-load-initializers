2dfb37744431831f9ce0ae64cbaa24a6f79a8445
Josemar Luedke <josemarluedke@gmail.com>
2016-02-02 18:53:30 -0800

    Add shims for ember/load-initializers

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-load-initializers'
+  name: 'ember-load-initializers',
+
+  included: function() {
+    this._super.included.apply(this, arguments);
+
+    this.app.import('vendor/ember-load-initializers/legacy-shims.js');
+  }
 };
