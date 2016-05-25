03ded759d4d02fdc0f9ce2668d8047422b9cb3ed
Krati Ahuja <krahuja@linkedin.com>
2016-05-25 14:39:49 -0700

    Avoid double walking of all modules
    Currently this addon walks all of the modules twice in order to determine the modules that are initializers and instance initializers. We can squash the `map` and `filter` loops into one.
    
    This PR addresses that.

--- addon/index.js
+++ addon/index.js
@@ -5,34 +5,24 @@ export default function(app, prefix) {
   var getKeys = (Object.keys || Ember.keys);
   var moduleNames = getKeys(requirejs._eak_seen);
 
-  var deps = [];
   for (var i = 0; i < moduleNames.length; i++) {
     var moduleName = moduleNames[i];
     var matches = regex.exec(moduleName);
 
     if (matches && matches.length === 2) {
-      deps.push({
-        moduleName: moduleName,
-        matches: matches
-      });
+      var module = require(moduleName, null, null, true);
+      if (!module) { throw new Error(moduleName + ' must export an initializer.'); }
+
+      var initializerType = Ember.String.camelize(matches[1].substring(0, matches[1].length - 1));
+      var initializer = module['default'];
+      if (!initializer.name) {
+        var initializerName = moduleName.match(/[^\/]+\/?$/)[0];
+        initializer.name = initializerName;
+      }
+
+      if (app[initializerType]) {
+        app[initializerType](initializer);
+      }
     }
   }
-
-  deps.forEach(function(dep) {
-    var moduleName = dep.moduleName;
-
-    var module = require(moduleName, null, null, true);
-    if (!module) { throw new Error(moduleName + ' must export an initializer.'); }
-
-    var initializerType = Ember.String.camelize(dep.matches[1].substring(0, dep.matches[1].length - 1));
-    var initializer = module['default'];
-    if (!initializer.name) {
-      var initializerName = moduleName.match(/[^\/]+\/?$/)[0];
-      initializer.name = initializerName;
-    }
-
-    if (app[initializerType]) {
-      app[initializerType](initializer);
-    }
-  });
 }
