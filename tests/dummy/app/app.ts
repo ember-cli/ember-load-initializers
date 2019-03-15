74ad0627fe91c16f1ba5f9b3ab0a838f6870e293
Ruslan Hrabovyi <kudgo.test@gmail.com>
2019-03-16 01:06:29 +0200

    Add typescript support

--- tests/dummy/app/app.ts
+++ tests/dummy/app/app.ts
@@ -3,12 +3,20 @@ import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
+declare global {
+  interface Window {
+    fooInitializeWasCalled: any;
+    barInitializeWasCalled: any;
+  }
+}
+
 const App = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver,
   destroy() {
     this._super(...arguments);
+
     delete self.fooInitializeWasCalled;
     delete self.barInitializeWasCalled;
   }
